38a9e994cb8a4ed9d16be1da5d93b522c7dbfadb
renfufei <renfufei@qq.com>
2015-11-20 11:18:12 +0800

    简单初始版本

---
+++ app/src/main/java/com/cncounter/cncounter/DisplayMessageActivity.java
@@ -0,0 +1,46 @@
+package com.cncounter.cncounter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        //
+        Intent intent = super.getIntent();
+        String message = intent.getExtras().getString(MainActivity.EXTRA_MESSAGE);
+        //
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        //
+        RelativeLayout view = (RelativeLayout)findViewById(R.id.activity_display_message_view);
+        //
+        view.addView(textView);
+
+        //
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
---
+++ app/src/main/java/com/cncounter/cncounter/MainActivity.java
@@ -0,0 +1,78 @@
+package com.cncounter.cncounter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.cncounter.cncounter.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //
+        MenuInflater inflater = super.getMenuInflater();
+        //
+        inflater.inflate(R.menu.menu_main, menu);
+        //
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //openSettings();
+            return true;
+        } else if (id == R.id.action_search) {
+            //openSettings();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) super.findViewById(R.id.edit_message);
+
+        String message = editText.getText().toString();
+        //
+        intent.putExtra(EXTRA_MESSAGE, message);
+        super.startActivity(intent);
+
+    }
+}
